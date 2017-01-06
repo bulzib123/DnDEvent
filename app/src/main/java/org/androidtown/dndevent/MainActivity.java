c55eedc1a0488743522202161f28e3d32ec7cd92
DJPhang <bulzib123@gmail.com>
2017-01-06 17:43:01 +0900

    첫 번째 Commit ~!

---
+++ app/src/main/java/org/androidtown/dndevent/MainActivity.java
@@ -0,0 +1,51 @@
+package org.androidtown.dndevent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ToggleButton;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    //깃헙
+    ViewFlipper flipper;
+
+    //자동 Flipping 선택 ToggleButton 참조변수
+    ToggleButton toggle_Flipping;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //ViewFlipper 객체 참조
+        flipper = (ViewFlipper) findViewById(R.id.flipper);
+
+
+        //ViewFlipper가 View를 교체할 때 애니메이션이 적용되도록 설정
+        //애니메이션은 안드로이드 시스템이 보유하고 있는  animation 리소스 파일 사용.
+        //ViewFlipper의 View가 교체될 때 새로 보여지는 View의 등장 애니메이션
+        //AnimationUtils 클래스 : 트윈(Tween) Animation 리소스 파일을 Animation 객체로 만들어 주는 클래스
+        //AnimationUtils.loadAnimaion() - 트윈(Tween) Animation 리소스 파일을 Animation 객체로 만들어 주는 메소드
+        //첫번째 파라미터 : Context
+        //두번재 파라미터 : 트윈(Tween) Animation 리소스 파일(여기서는 안드로이드 시스템의 리소스 파일을 사용
+        //                    (왼쪽에서 슬라이딩되며 등장)
+        Animation showIn = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        //ViewFlipper에게 등장 애니메이션 적용
+        flipper.setInAnimation(showIn);
+
+        //ViewFlipper의 View가 교체될 때 퇴장하는 View의 애니메이션
+        //오른쪽으로 슬라이딩 되면 퇴장하는 애니메이션 리소스 파일 적용.
+        //위와 다른 방법으로 애니메이션을 적용해봅니다.
+        //첫번째 파라미터 : Context
+        //두번재 파라미터 : 트윈(Tween) Animation 리소스 파일(오른쪽으로 슬라이딩되며 퇴장)
+        flipper.setOutAnimation(this, android.R.anim.slide_out_right);
+
+        flipper.setFlipInterval(2500);//플리핑 간격(1000ms)
+        flipper.startFlipping();//자동 Flipping 시작
+
+
+    }
+}
